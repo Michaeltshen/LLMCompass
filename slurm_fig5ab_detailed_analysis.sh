08f4606ac8e59134107996a100c18d18e160f7e3
michaeltshen <mts247@cornell.edu>
2024-07-09 22:58:45 -0400

    Data overhaullll

---
+++ slurm_fig5ab_detailed_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name llmcompass-gpu                         # Job name
+#SBATCH --mail-type=ALL                      # Request status by email 
+#SBATCH --mail-user=mts247@cornell.edu        # Email address to send results to.
+#SBATCH --get-user-env
+#SBATCH --mem=100G
+#SBATCH -t 99:00:00
+#SBATCH --partition=default_partition
+#SBATCH --requeue
+
+python -m ae.figure5.ab.test_matmul --simgpu
